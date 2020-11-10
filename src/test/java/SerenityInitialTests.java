50107a471554fc56b576dc72fca09b80696f1daa
Walter <U201620367@upc.edu.pe>
2020-11-10 15:40:11 -0500

    agregar tasks para post

--- src/test/java/SerenityInitialTests.java
+++ src/test/java/SerenityInitialTests.java
@@ -1,4 +1,5 @@
 import models.users.Datum;
+import models.users.RegisterUserInfo;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith;
 import questions.GetUsersQuestion;
 import questions.ResponseCode;
 import tasks.GetUsers;
+import tasks.RegisterUser;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -38,6 +40,40 @@ public class SerenityInitialTests {
                 seeThat("el primer nombre del usuario", act -> user.getFirstName(), equalTo("George")),
                 seeThat("el apellido del usuario", act -> user.getLastName(), equalTo("Bluth")),
                 seeThat("el avantar del usuario", act -> user.getAvatar(), equalTo("https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"))
+        );
+    }
+
+    @Test
+    public void registerUserTest() {
+        Actor walter = Actor.named("Walter").whoCan(CallAnApi.at(restApiUrl));
+
+        String registerUserInfo = "{\"name\":\"Angel\",\"job\":\"leader\",\"email\":\"eve.holt@reqres.in\",\"password\":\"pistol\"}";
+
+        walter.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+        );
+
+        walter.should(
+                seeThat("el código de respuesta", ResponseCode.was(), equalTo(200))
+        );
+    }
+
+    @Test
+    public void registerUserTest2() {
+        Actor walter = Actor.named("Walter").whoCan(CallAnApi.at(restApiUrl));
+
+        RegisterUserInfo registerUserInfo = new RegisterUserInfo();
+        registerUserInfo.setName("Wacho");
+        registerUserInfo.setJob("leader");
+        registerUserInfo.setEmail("eve.holt@reqres.in");
+        registerUserInfo.setPassword("serenity");
+
+        walter.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+        );
+
+        walter.should(
+                seeThat("el código de respuesta", ResponseCode.was(), equalTo(200))
         );
     }
 }
